26d101c1dc92fc6809b4948bae93a6a4fd850bc8
json <therealcannedtuna@gmail.com>
2026-03-23 13:59:51 -0700

    Updated test

--- python/test.py
+++ python/test.py
@@ -6,9 +6,22 @@ from ultralytics import YOLO
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--model", required=True, help="Model to load")
+parser.add_argument("--model", help="Model to load. If none, defaults to 'yolo26x.pt'",
+                    default='yolo216x.pt')
 parser.add_argument("--input", required=True, help="Input video path")
 parser.add_argument("--output", required=True, help="Output video path")
+parser.add_argument('--show_conf', default=False, action='store_true',
+                    help='Whether to show the confidence scores')
+parser.add_argument('--show_labels', default=False,
+                    action='store_true', help='Whether to show the labels')
+parser.add_argument('--conf', type=float, default=0.5,
+                    help='Object confidence threshold for detection')
+parser.add_argument('--classes', nargs='+', default=None,
+                    help='List of classes to detect')
+parser.add_argument('--line_width', type=int, default=3,
+                    help='Line width for bounding box visualization')
+parser.add_argument('--font_size', type=float, default=3,
+                    help='Font size for label visualization')
 
 args = parser.parse_args()
 
@@ -28,13 +41,23 @@ def main():
         ret, frame = cap.read()
         if not ret:
             break
-        results = model.predict(
+        results = model.track(
             frame,
+            conf=args.conf,
             classes=[0, 32], # [human, sportsball]
+            tracker='botsort.yaml',
+            with_reid=True,
+            persist=True,
+            verbose=False,
         )
 
         annotated_frame = frame.copy()
-        annotated_frame = results[0].plot()
+        annotated_frame = results[0].plot(
+            conf=args.show_conf,
+            labels=args.show_labels,
+            line_width=args.line_width,
+            font_size=args.font_size,
+        )
 
         out.write(annotated_frame)
 
