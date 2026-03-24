2116ba93b5b9651d5e1d107ac5572265ad1e724a
Evan Inrig <inrig003@gmail.com>
2026-03-23 20:54:27 -0700

    changing fonts

--- volleyball-vision/src/components/BackgroundVideo.jsx
+++ volleyball-vision/src/components/BackgroundVideo.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import videos from '../assets/videos/volleyball-footage.mp4';
 
 function BackgroundVideo(){
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.playbackRate = 0.65;
+        }
+    }, []);
+
     return (
-        <video className='absolute z-0 w-auto min-w-full min-h-full max-w-none blur-xs' autoPlay loop muted>
+        <video ref={videoRef} className='absolute z-0 w-auto min-w-full min-h-full max-w-none blur-xs' autoPlay loop muted>
             <source src={videos} type='video/mp4' />
         </video>    
     );
--- volleyball-vision/src/pages/LandingPage.jsx
+++ volleyball-vision/src/pages/LandingPage.jsx
@@ -8,14 +8,16 @@ import BackgroundVideo from '../components/BackgroundVideo';
 function LandingPage(){
     return (
         <>
-            <div id="background-video" className="flex items-center justify-center h-screen overflow-hidden">
+            <div id="background-video" className="relative flex flex-col items-center justify-center h-screen overflow-hidden">
                 <BackgroundVideo />
-                <div className="z-10 p-4 text-center  shadow-lg-20">
+                <div className="z-10 p-4 text-center shadow-lg-20">
                     <h1 className="text-orange-300 text-shadow-lg/20 text-8xl font-bold">VolleyVision</h1>
                     <h2 className="text-orange-200/90 text-shadow-lg/20 text-2xl font-semibold">By Evan Inrig, Kyumin Han, and Jason Press</h2>
                 </div>
-                <button className="bg-slate-900">
-                    Get Started
+                <button className="z-10 bg-app-ice-blue p-5">
+                    <h1 className="text-2xl font-bold">
+                        Get Started
+                    </h1>
                 </button>
             </div>
         </>
