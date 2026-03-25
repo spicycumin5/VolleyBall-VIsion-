7dd7d9fd45968a6377dadcef7b477fecc8cbd2ab
Evan Inrig <inrig003@gmail.com>
2026-03-25 10:50:05 -0700

    youtube-esque layout

--- volleyball-vision/src/components/Dashboard/Thumbnail.jsx
+++ volleyball-vision/src/components/Dashboard/Thumbnail.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function Thumbnail({ video }){
+function Thumbnail({ video={
+    src: "https://www.fivb.com/wp-content/uploads/2025/07/102195-1.jpeg", 
+    title: "Replay 01",
+    date: "March 25, 10:15 AM",
+} }){
     return (
-        <div className='grid'>
-            <img></img>
+        <div className='hover:bg-blue-800/20 rounded-xl transition-all duration-150 p-2'>
+            <img src={video.src} className='rounded-xl'/>
             <div>
                 <h1> {video.title}</h1>
                 <h2> {video.date} </h2>
--- volleyball-vision/src/pages/Dashboard.jsx
+++ volleyball-vision/src/pages/Dashboard.jsx
@@ -4,17 +4,42 @@
 
 import React from 'react';
 import Toolbar from '../components/Dashboard/Toolbar';
+import Thumbnail from '../components/Dashboard/Thumbnail';
 
 function Dashboard(){
-    return(
-        <div className="flex flex-col min-h-screen min-w-screen bg-app-ice-blue gap-0">
+    return (
+    <div className="flex flex-col h-screen min-w-screen bg-app-ice-blue overflow-hidden">
+        
+        {/* Fixed Header */}
+        <div className="flex-none z-10">
             <Toolbar />
-            <div className='grow flex flex-row gap-2'>
-                <div className='basis-1/5 bg-white'> 02 </div>
-                <div className='basis-4/5 bg-white'> 03 </div>
+        </div>
+
+        <div className="flex flex-row flex-1 overflow-hidden">
+
+            {/* Fixed Left Bar */}
+            <div className="flex-none sm:hidden md:block md:w-1/5 rounded-tr-xl bg-white overflow-y-auto">
+                02
+            </div>
+
+            {/* Scrollable Videos Grid */}
+            <div className="flex-1 grid overflow-y-auto sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-2 px-4 py-1 content-start">
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
             </div>
         </div>
-        );
+    </div>
+);
 }
 
 export default Dashboard;
